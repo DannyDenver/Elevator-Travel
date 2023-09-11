12ca685798521618de5c7fd78670f70db2e33c86
Daniel Taylor <djt09d@fulbrightmail.org>
2023-09-11 09:38:10 -0600

    Update elevator_travel.test.ts

--- tests/elevator_travel.test.ts
+++ tests/elevator_travel.test.ts
@@ -1,21 +1,20 @@
-import {describe, expect, test} from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import { elevator_travel_time_loop } from '../src/elevator_travel_loop';
 import { elevator_travel_time_functional } from '../src/elevator_travel_functional';
 import { elevator_travel_time_pointer } from '../src/elevator_travel_pointers';
 import { elevator_travel_time_recursive } from '../src/elevator_travel_recursive';
 
-describe('test index', () => {
+describe('elevator travel time', () => {
     [elevator_travel_time_loop, elevator_travel_time_functional, elevator_travel_time_pointer, elevator_travel_time_recursive].forEach(fn => {
         test('function works', () => {
-            expect(fn(12,[2,9,1,32])).toStrictEqual([560,[12,2,9,1,32]])
-         })
-        test('function works', () => {
-            expect(fn(5,[1, 8, 4, 1])).toStrictEqual([180,[5,1,8,4,1]])
+            expect(fn(12, [2, 9, 1, 32])).toStrictEqual([560, [12, 2, 9, 1, 32]])
         })
-    
         test('function works', () => {
-            expect(fn(6,[5, 2, 8, 1])).toStrictEqual([170,[6,5,2,8,1]])
+            expect(fn(5, [1, 8, 4, 1])).toStrictEqual([180, [5, 1, 8, 4, 1]])
+        })
+
+        test('function works', () => {
+            expect(fn(6, [5, 2, 8, 1])).toStrictEqual([170, [6, 5, 2, 8, 1]])
         })
     })
-
 })
